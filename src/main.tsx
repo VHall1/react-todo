c72ca51613de02f49929ac788f27b36ec24e4e39
VHall1 <victorhallpsn@hotmail.com>
2023-08-10 21:47:09 +0100

    consistent file name casing

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import { store } from "@/store";
 import { Provider } from "react-redux";
-import { App } from "./App.tsx";
+import { App } from "./app";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
